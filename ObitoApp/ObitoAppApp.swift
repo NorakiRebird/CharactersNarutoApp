94d2e5a511d3e5df76fe6b1c89080cfe2014ebd7
NorakiRebird <Rayann_Chaghla@outlook.fr>
2025-02-24 20:10:00 +0100

    Initial Commit

---
+++ ObitoApp/ObitoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObitoAppApp.swift
+//  ObitoApp
+//
+//  Created by Rayann chaghla on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ObitoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
